60255e4ddb8d3d044f08aa2a497f320b6cf0b304
Shreethaar Arunagirinathan <64171155+Shreethaar@users.noreply.github.com>
2023-10-18 19:55:46 +0800

    Add files via upload

---
+++ randomNumberGenerator.java
@@ -0,0 +1,13 @@
+//Generating a random number within a range
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * randomNumberGenerator
+ */
+public class randomNumberGenerator {
+
+    public static void main(String[] args) {
+        
+    }
+}
